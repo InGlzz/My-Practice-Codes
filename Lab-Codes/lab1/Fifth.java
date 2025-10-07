eccbbe7da710b23f9758b8432bac163e11a1ad1b
InGlzz <hamzagedik369077@gmail.com>
2025-10-07 22:38:54 +0300

    Just publishing

---
+++ Lab-Codes/lab1/Fifth.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Third{
+     public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+     System.out.println("Whats the first quiz note?");
+	  float firstQuiz = sc.nextFloat();
+     System.out.println("Whats the second quiz note?");
+          float secondQuiz = sc.nextFloat();
+     System.out.println("Whats the Third quiz note?");
+	  float thirdQuiz = sc.nextFloat();
+
+    float allQuizes = firstQuiz + secondQuiz + thirdQuiz;
+	  float quizCount = allQuizes * 40 / 100;
+
+     System.out.println("Whats your Final exam note?");
+     float finalQuiz = sc.nextFloat();
+	 float countedFinalQuiz = finalQuiz * 60 / 100;
+
+	 float theNote = countedFinalQuiz + quizCount;
+	     System.out.println("Your note is: " + theNote);
+   }
+}
---
+++ Lab-Codes/lab1/Fourth.java
@@ -0,0 +1,12 @@
+public class Third{
+     public static void main(String[] args){
+int x = 5;
+System.out.println(x++); // post-increment
+System.out.println(++x); // pre-increment
+int x = 3;
+int y = x++ + ++x + x--;
+System.out.println("x = " + x + ", y = " + y);
+
+  }
+}
+//Teacher wrote jsut check
